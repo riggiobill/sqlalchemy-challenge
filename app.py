0a5cfa13585b45e7b39418942ba922966f690558
BillRigg <riggiobill@gmail.com>
2020-10-06 18:46:04 -0400

    Home Route and More
    Home Route and setup in comments for others

--- app.py
+++ app.py
@@ -9,6 +9,16 @@ app = Flask(__name__)
 ## see: app w/ querying notes
 ##
 
+##session engine sqlite file
+##query engine in same way as other file
+##save tables as classes
+
+##create info variables for Measurement and Station to use below
+
+
+
+
+
 @app.route("/")
 def home():
     return (
@@ -22,8 +32,22 @@ def home():
         f""
     )
 
+
+
+
+##basically, the below are each the same calls as the other portion of the homework
+##pull from that and it'll be done in no time.
+
 @app.route("/precipitation")
 def about():
+
+    ##query for precipitation for last 12 months
+    ##as per other file
+    ##instantiated as a DICT
+    ##jsonify?
+
+
+
     name = ""
     current_location = ""
     return f"{name} {current_location}"
